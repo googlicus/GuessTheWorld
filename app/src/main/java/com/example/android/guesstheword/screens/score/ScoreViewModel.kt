f40c5e3472adfa44af7fbb4288fe7bafa1cac5c2
googlicus <googlicus@gmail.com>
2020-12-26 21:20:30 +0500

    again button

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -10,8 +10,19 @@ class ScoreViewModel(finalScore: Int) : ViewModel() {
     val score: LiveData<Int>
         get() = _score
 
+    private val _eventPlayAgain = MutableLiveData<Boolean>()
+    val eventPlayAgain: LiveData<Boolean>
+        get() = _eventPlayAgain
+
     init {
         _score.value = finalScore
         Log.i("ScoreViewModel", "Final score is $finalScore")
+    }
+
+    fun onPlayAgain() {
+        _eventPlayAgain.value = true
+    }
+    fun onPlayAgainComplete() {
+        _eventPlayAgain.value = false
     }
 }
